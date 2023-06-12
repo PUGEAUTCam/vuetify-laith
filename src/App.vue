30770dbfd50508031d9b6de12f3474ee19218df3
Camille Pugeaut <camille.pugeaut@gmail.com>
2023-06-12 17:52:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <Sidebar @selectOption="onSelectOption" />
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import ImageGallery from "./components/ImageGallery.vue";
+import Sidebar from "./components/Sidebar.vue";
+import { ref } from "vue";
+
+const isWithColor = ref(true);
+
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor;
+};
+</script>
